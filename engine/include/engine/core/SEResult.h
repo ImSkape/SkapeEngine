3212665175c395a6f4bc79f2ab9deec986b176a4
Skape <127537688+ImSkape@users.noreply.github.com>
2026-05-07 17:22:04 -0700

    platform fix check

--- engine/include/engine/core/SEResult.h
+++ engine/include/engine/core/SEResult.h
@@ -10,12 +10,34 @@
 #include <cstdlib>
 
 #ifdef ENGINE_DEBUG
+
+
+#if defined(PLATFORM_WINDOWS)
 #define ENGINE_ASSERT(condition, msg) \
     if (!(condition)) { \
         printf("[ASSERT FAILED] %s\n  File: %s\n  Line: %d\n", \
                msg, __FILE__, __LINE__); \
         __debugbreak(); \
     }
+#elif defined(PLATFORM_MAC)
+#define ENGINE_ASSERT(condition, msg) \
+    if (!(condition)) { \
+        printf("[ASSERT FAILED] %s\n  File: %s\n  Line: %d\n", \
+               msg, __FILE__, __LINE__); \
+        __builtin_debugtrap(); \
+    }
+#elif defined(PLATFORM_LINUX)
+#define ENGINE_ASSERT(condition, msg) \
+    if (!(condition)) { \
+        printf("[ASSERT FAILED] %s\n  File: %s\n  Line: %d\n", \
+               msg, __FILE__, __LINE__); \
+        __builtin_trap(); \
+    }
+
+#else
+#define ENGINE_ASSERT(condition, msg)// compiled out in other OS
+#endif
+
 #else
 #define ENGINE_ASSERT(condition, msg) // compiled out in Release
 #endif
